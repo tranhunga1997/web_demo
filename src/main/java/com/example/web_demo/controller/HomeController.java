a02a74d633b1f3fa75d79e187297fa12f1812489
MANH HUNG <tranhunga1997@gmail.com>
2021-09-19 03:34:52 +0900

    Initial commit

---
+++ src/main/java/com/example/web_demo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.web_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("")
+    @ResponseBody
+    public String Home(){
+        return "Hello World";
+    }
+}
